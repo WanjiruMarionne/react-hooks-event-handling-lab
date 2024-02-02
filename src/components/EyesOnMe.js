a560048fc1f8e404cf040d20ba337df28bc3a5f2
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-02-02 09:19:14 +0300

    Completed Event Handling in React Lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe() {
+  const focusEvent = () => {
+    console.log('Good!');
+  };
+
+  const blurEvent = () => {
+    console.log('Hey! Eyes on me!');
+  };
+
+  return (
+    <button onFocus={focusEvent} onBlur={blurEvent}>
+      Eyes on me
+    </button>
+  );
+}
+
+export default EyesOnMe;
